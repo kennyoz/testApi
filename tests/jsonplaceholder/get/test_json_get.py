393fd9190dea1d4a91667a0d4deacd2639051fd3
kennyoz <ihorkenny@yandex.ru>
2024-11-12 22:44:40 +0300

    del json files

---
+++ tests/jsonplaceholder/get/test_json_get.py
@@ -0,0 +1,16 @@
+import requests
+import pytest
+
+
+def test_jsonplaysholder_get():
+    url = "https://jsonplaceholder.typicode.com/todos/1"
+    response = requests.get(url)
+    actual = response.json()
+    expected = {
+    "userId": 1,
+    "id": 1,
+    "title": "delectus aut autem",
+    "completed": False
+}
+    assert response.status_code == 200," Invalid status code"
+    assert actual == expected,"The response body is incorrect"
