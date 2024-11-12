c0817a29591e46d7d2a7b3da55f27376a3b84d5e
kennyoz <ihorkenny@yandex.ru>
2024-11-12 22:31:58 +0300

    del json files

--- tests/Starwars/Test_id/positive/Luke/test_Luke_Skywalker.py
+++ tests/Starwars/Test_id/positive/Luke/test_Luke_Skywalker.py
@@ -12,8 +12,35 @@ def test_luke_skywalker():
 
     actual_data = response.json()
 
-    with open("C:/Users/kenya/PycharmProjects/testApi/tests/Starwars/Test_id/positive/Luke/Get_First_person.json", "r") as f:
-        expected_data = json.load(f)
 
+    expected_data = {
+    "name": "Luke Skywalker",
+    "height": "172",
+    "mass": "77",
+    "hair_color": "blond",
+    "skin_color": "fair",
+    "eye_color": "blue",
+    "birth_year": "19BBY",
+    "gender": "male",
+    "homeworld": "https://swapi.dev/api/planets/1/",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/6/"
+    ],
+    "species": [],
+    "vehicles": [
+        "https://swapi.dev/api/vehicles/14/",
+        "https://swapi.dev/api/vehicles/30/"
+    ],
+    "starships": [
+        "https://swapi.dev/api/starships/12/",
+        "https://swapi.dev/api/starships/22/"
+    ],
+    "created": "2014-12-09T13:50:51.644000Z",
+    "edited": "2014-12-20T21:17:56.891000Z",
+    "url": "https://swapi.dev/api/people/1/"
+}
     assert actual_data == expected_data, "API response does not match the expected JSON"
 
--- tests/Starwars/Test_id/positive/R2_D2/test_R2_D2.py
+++ tests/Starwars/Test_id/positive/R2_D2/test_R2_D2.py
@@ -7,6 +7,32 @@ def test_R2_D2():
     response = requests.get(url)
     assert response.status_code == 200
     actual_data = response.json()
-    with open("C:/Users/kenya/PycharmProjects/testApi/tests/Starwars/Test_id/positive/R2_D2/R2_D2.json", "r") as f:
-        expected_data = json.load(f)
+
+    expected_data = {
+    "name": "R2-D2",
+    "height": "96",
+    "mass": "32",
+    "hair_color": "n/a",
+    "skin_color": "white, blue",
+    "eye_color": "red",
+    "birth_year": "33BBY",
+    "gender": "n/a",
+    "homeworld": "https://swapi.dev/api/planets/8/",
+    "films": [
+        "https://swapi.dev/api/films/1/",
+        "https://swapi.dev/api/films/2/",
+        "https://swapi.dev/api/films/3/",
+        "https://swapi.dev/api/films/4/",
+        "https://swapi.dev/api/films/5/",
+        "https://swapi.dev/api/films/6/"
+    ],
+    "species": [
+        "https://swapi.dev/api/species/2/"
+    ],
+    "vehicles": [],
+    "starships": [],
+    "created": "2014-12-10T15:11:50.376000Z",
+    "edited": "2014-12-20T21:17:50.311000Z",
+    "url": "https://swapi.dev/api/people/3/"
+}
     assert actual_data == expected_data, "API response does not match the expected JSON"
